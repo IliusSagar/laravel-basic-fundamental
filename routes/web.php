8185d7c99f60a03e2a4d1019e345308900c8f9f4
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-11-27 17:27:52 +0600

    routes laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+// Route::get('/', function () {
+//     return "<h1>Hello World!</h1>";
+// });
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/feed', function () {
+    return view('feed');
+});
+
+Route::get('/profile', function () {
+    return view('users.profile');
 });
