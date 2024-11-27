bcfa38ce1d4e8071f4efe20f0078aed5515fde9f
Md. Ilius Sagar <89338796+IliusSagar@users.noreply.github.com>
2024-11-27 22:56:57 +0600

    Global Middleware

---
+++ app/Http/Middleware/WebGuard.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class WebGuard
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // echo "Hello";
+
+        // echo $request->age;
+
+        if($request->age < 18){
+            echo "You're not allow to access the page";
+            die;
+        }
+
+
+        return $next($request);
+    }
+}
